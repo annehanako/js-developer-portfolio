c9baf8436eb3620b89a42c8eccb866fc00c5fd99
annehanako <anne.f.hanako@gmail.com>
2024-01-24 14:32:14 -0300

    fixing empty sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,30 @@
 function updateProfileInfo(profileData) {
-    const photo = document.getElementById('profile.photo')
-    photo.src = profileData.photo
-    photo.alt = profileData.name
+    const photo = document.getElementById('profile.photo');
+    photo.src = profileData.photo;
+    photo.alt = profileData.name;
 
-    const name = document.getElementById('profile.name')
-    name.innerText = profileData.name
+    const name = document.getElementById('profile.name');
+    name.innerText = profileData.name;
 
-    const job = document.getElementById('profile.job')
-    job.innerText = profileData.job
+    const job = document.getElementById('profile.job');
+    job.innerText = profileData.job;
 
-    const location = document.getElementById('profile.location')
-    location.innerText = profileData.location
+    const location = document.getElementById('profile.location');
+    location.innerText = profileData.location;
 
-    const phone = document.getElementById('profile.phone')
-    phone.innerText = profileData.phone
-    phone.href = `tel:${profileData.phone}`
+    const phone = document.getElementById('profile.phone');
+    phone.innerText = profileData.phone;
+    phone.setAttribute('href', `tel:${profileData.phone}`);
 
-    const email = document.getElementById('profile.email')
-    email.innerText = profileData.email
-    email.href = `mailto:${profileData.email}`
+    const email = document.getElementById('profile.email');
+    email.innerText = profileData.email;
+    email.setAttribute('href', `mailto:${profileData.email}`);
 }
 
+
 function updateSoftSkills(profileData) {
-    const softSkills = document.getElementById('profile.skills.softSkills')
-    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+    const softSkills = document.getElementById('profile.skills.softSkills');
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('');
 }
 
 function updateHardSkills(profileData) {
